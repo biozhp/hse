4fcad38004bcdde14752ea5333fe19f50f9580f3
Peng Zhao <51162669+biozhp@users.noreply.github.com>
2019-12-14 20:30:07 +0800

    Update run.sh

--- example/hse/run.sh
+++ example/hse/run.sh
@@ -1,3 +1,6 @@
 ## Genome wide identification of heat shock elements
 sh ./run_pipeline.sh ./example/hse/wheat.V1.cds.fa wheat.V1.gene.gff3 wheat_genome_chr_length.txt wheat.V1.genome.fa ./wheat_hse
-## Because the wheat.V1.genome.fa is more than 14G and cannot be uploaded, the genome sequence is not stored in this directory. It can be downloaded on the website(https://urgi.versailles.inra.fr/download/iwgsc/IWGSC_RefSeq_Assemblies/v1.0/iwgsc_refseqv1.0_all_chromosomes.zip).
+## Because the wheat.V1.cds.fa, wheat.V1.gene.gff3 and wheat.V1.genome.fa is so big and cannot be uploaded, this files is not stored in this directory. 
+## Wheat.V1.cds.fa can be downloaded on the website(https://urgi.versailles.inra.fr/download/iwgsc/IWGSC_RefSeq_Annotations/v1.0/iwgsc_refseqv1.0_HighConf_CDS_2017Mar13.fa.zip).
+## Wheat.V1.gene.gff3 can be downloaded on the website(https://urgi.versailles.inra.fr/download/iwgsc/IWGSC_RefSeq_Annotations/v1.0/iwgsc_refseqv1.0_HighConf_2017Mar13.gff3.zip).
+## Wheat.V1.genome.fa can be downloaded on the website(https://urgi.versailles.inra.fr/download/iwgsc/IWGSC_RefSeq_Assemblies/v1.0/iwgsc_refseqv1.0_all_chromosomes.zip).
